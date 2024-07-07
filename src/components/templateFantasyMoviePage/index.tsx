909fa87348751c41b85aa58e90ab40816a4a95ab
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-07-07 15:45:45 +0100

    Fantasy Movie Functionality: working on the form

--- src/components/templateFantasyMoviePage/index.tsx
+++ src/components/templateFantasyMoviePage/index.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import Header from "../headerMovieList";
 import Grid from "@mui/material/Grid";
-import {  BaseFantasyMovieProps} from "../../types/interfaces";
-import PeopleList from "../peopleList";
-
+import {BaseFantasyMovieProps, MovieDetailsProps} from "../../types/interfaces";
 
 const styles = {
     root: {
         backgroundColor: "#bfbfbf",
     }
 };
-
-const FantasyMoviePageTemplate: React.FC<BaseFantasyMovieProps> = ({ title})=> {
+interface TemplateFantasyPageProps {
+    pageTitle: "string";
+    fantasy: BaseFantasyMovieProps;
+    children: React.ReactElement;
+}
+const FantasyMoviePageTemplate: React.FC<TemplateFantasyPageProps> = ({ fantasy, pageTitle,  children })=> {
     return (
         <Grid container sx={styles.root}>
             <Grid item xs={12}>
-                <Header title={title} />
+                <Header title={pageTitle} />
 
+            </Grid>
+
+            <Grid item xs={9}>
+                {children}
             </Grid>
         </Grid>
     );
--- src/pages/fantasyMoviePage.tsx
+++ src/pages/fantasyMoviePage.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import PageTemplate from "../components/templateFantasyMoviePage";
-import MovieReview from "../components/movieReview";
-import {useLocation} from "react-router-dom";
-import styles from "../components/reviewForm/styles.ts";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
-
+import FantasyMovieForm from "../components/fantasyMovieForm";
 const FantasyMoviePage: React.FC = () => {
-
     return (
         <>
-            <PageTemplate
-                title="Fantacy Movie"
-
-            />
-
-            <Box component="div" sx={styles.root}>
-                <Typography component="h2" variant="h3">
-                    Create a fantacy movie
-                </Typography>
-            </Box>
-
-
+            <PageTemplate pageTitle="Fantasy Movie">
+                <FantasyMovieForm />
+            </PageTemplate>
         </>
-
-
     );
 };
 export default FantasyMoviePage;
