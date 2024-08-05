030180f96412cecb28407867317da8c3505465bc
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-08-05 12:24:28 +0100

    Vercel Deployment: Error fix

--- src/fakeAuth.tsx
+++ src/fakeAuth.tsx
@@ -1,5 +1,5 @@
 
-const fakeAuth =  async (username:string,password:string) =>
+const fakeAuth =  async (username:string, password:string) =>
     new Promise<string>((resolve) => {
         setTimeout(() => resolve('2342f2f1d131rf12'), 250);
     });
