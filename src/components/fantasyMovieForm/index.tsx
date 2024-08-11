9def1d26563a546542fc5536ef35ab79d84756df
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-08-11 13:33:16 +0100

    Enhancing Fantasy Movies feature: added GenAI to see version of fantasy like Quentin Tarantino would do it

--- src/components/fantasyMovieForm/index.tsx
+++ src/components/fantasyMovieForm/index.tsx
@@ -18,6 +18,7 @@ import { v4 as uuidv4 } from 'uuid';
 import LlamaAI from 'llamaai';
 import LinearProgress from '@mui/material/LinearProgress';
 import BootstrapDialog from "../dialogButton";
+import ImageUpload from "../imageUpload/index.jsx";
 
 const apiToken = import.meta.env.VITE_API_TOKEN;
 const llamaAPI = new LlamaAI(apiToken);
@@ -116,7 +117,14 @@ const FantasyMovieForm: React.FC<BaseFantasyMovieProps> = () => {
             });
     };
 
-
+    const galleryImageList = [
+        "https://raw.githubusercontent.com/dxyang/StyleTransfer/master/style_imgs/mosaic.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg",
+        "https://raw.githubusercontent.com/ShafeenTejani/fast-style-transfer/master/examples/dora-maar-picasso.jpg",
+        "https://pbs.twimg.com/profile_images/925531519858257920/IyYLHp-u_400x400.jpg",
+        "https://raw.githubusercontent.com/ShafeenTejani/fast-style-transfer/master/examples/dog.jpg",
+        "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"
+    ];
     return (
         <Box component="div" sx={styles.root}>
             <Typography component="h2" variant="h3">
